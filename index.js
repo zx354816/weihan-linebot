0174b6519c6417095d087784b4cb1245b0a1c062
weihan.chu <qap66990070@gmail.com>
2018-06-06 09:58:09 +0800

    新的爬蟲寫法

--- index.js
+++ index.js
@@ -27,32 +27,39 @@ app.post('/linewebhook', linebotParser);
 
 const result = []; // 建立一個儲存結果的容器
 const earthquake = function () {
-  request({
-    url: "http://www.cwb.gov.tw/V7/modules/MOD_EC_Home.htm", // 中央氣象局網頁
-    method: "GET"
-  }, function (error, response, body) {
-    if (error || !body) {
-      return;
-    }
-    const $ = cheerio.load(body); // 載入 body
-    
-    const table_tr = $(".BoxTable tr"); // 爬最外層的 Table(class=BoxTable) 中的 tr
+    request({
+        url: "https://www.ptt.cc/bbs/LoL/index.html", // LOOOL
+        method: "GET"
+    }, function (error, response, body) {
+        if (error || !body) {
+            return;
+        }
+        const $ = cheerio.load(body); // 載入 body
 
-    for (let i = 1; i < table_tr.length; i++) { // 走訪 tr
-      const table_td = table_tr.eq(i).find('td'); // 擷取每個欄位(td)
-      const time = table_td.eq(1).text(); // time (台灣時間)
-      const latitude = table_td.eq(2).text(); // latitude (緯度)
-      const longitude = table_td.eq(3).text(); // longitude (經度)
-      const amgnitude = table_td.eq(4).text(); // magnitude (規模)
-      const depth = table_td.eq(5).text(); // depth (深度)
-      const location = table_td.eq(6).text(); // location (位置)
-      const url = table_td.eq(7).text(); // url (網址)
-      // 建立物件並(push)存入結果
-      result.push(time);
-    }
-  });
+        const title_class = $(".title"); // 爬最外層的 Table(class=BoxTable) 中的 tr
+
+        //for (let i = 1; i < table_tr.length; i++) { // 走訪 tr
+        //  const table_td = table_tr.eq(i).find('td'); // 擷取每個欄位(td)
+        //  const time = table_td.eq(1).text(); // time (台灣時間)
+        //  const latitude = table_td.eq(2).text(); // latitude (緯度)
+        //  const longitude = table_td.eq(3).text(); // longitude (經度)
+        //  const amgnitude = table_td.eq(4).text(); // magnitude (規模)
+        //  const depth = table_td.eq(5).text(); // depth (深度)
+        //  const location = table_td.eq(6).text(); // location (位置)
+        //  const url = table_td.eq(7).text(); // url (網址)
+        //  // 建立物件並(push)存入結果
+        //  result.push(time);
+        //}
+        result.length = 0;
+        for (let i = 0 ; i < title_class.length - 4; i++) {
+            const title = title_class.eq(i).find('a').text();
+            result.push(title);
+        }
+
+
+        
+    });
 };
-earthquake();
 
 
 
@@ -127,16 +134,14 @@ bot.on('message', function (event) {
 			});
 			
 		}
-		else if(event.message.text == '地震'){
+		else if(event.message.text == 'LOL'){
 			
-			earthquake();
-			event.reply(result+"HI").then(function (data) {
+		    event.reply(result).then(function (data) {
                 
             }).catch(function (error) {
                 // error 
                 console.log('error');
             });
-			result.length=0;
 			
 		}
         /*
